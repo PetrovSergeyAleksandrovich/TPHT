2a0754a939b7a653c403ab3fc96c7aefb86379f3
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-08-09 18:34:10 +0300

    tmp commit2

---
+++ cmake-build-debug/CMakeFiles/TPHT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../cmake-build-debug}/TPHT.exe"
+  "../cmake-build-debug}/TPHT.exe.manifest"
+  "../cmake-build-debug}/TPHT.pdb"
+  "CMakeFiles/TPHT.dir/main.cpp.obj"
+  "TPHT.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TPHT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
