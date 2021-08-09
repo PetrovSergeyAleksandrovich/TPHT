43fafcef1c80f503f54f1f1041f379e3e259daa5
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-08-09 18:33:44 +0300

    tmp commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    cpr::Response resp1 = cpr::Get
+    (
+    cpr::Url("http://httpbin.org/get"),
+        cpr::Header
+            (
+                 {
+                    {"user-agent",
+                     "Mozilla/5.0"},
+
+                    {"Accept",
+                     "text/html"}
+                 }
+            )
+    );
+
+    std::cout << resp1.text << std::endl;
+
+    cpr::Response resp2 = cpr::Get(cpr::Url("http://httpbin.org/headers"));
+    std::cout << resp2.text << std::endl;
+
     return 0;
 }
